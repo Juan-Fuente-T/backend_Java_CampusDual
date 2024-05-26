66d94af53761aeaf1f878acb175e9799f57b3fff
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-05-26 19:19:51 +0200

    Todos los ejercicios y proyectos Java CampusDual

---
+++ Ejercicios_JUnit/Ejercicios_JUnit/src/test/java/junit/EjerciciosJUnit.java
@@ -0,0 +1,41 @@
+package junit;
+
+public class EjerciciosJUnit {
+    public static int sumEvenNum(int num) {
+        int total = 0;
+        for (int i = 1; i <= num; i++) {
+            total = total + 2 * i;
+        }
+        return total;
+    }
+
+    public static int factorial(int num) {
+        int total = 1;
+        for (int i = 1; i <= num; i++) {
+            total *= i; //o también total = total * i
+        }
+        return total;
+    }
+
+    public static int[] createAndInitializeArray(int numOfElements) {
+        int[] intArray = new int[numOfElements];
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i + 1;
+        }
+
+        return intArray;
+    }
+
+    public static String getUnidimensionalString(int[] uniArray) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < uniArray.length; i++) {
+            sb.append(uniArray[i]);
+            sb.append(" ");
+        }
+
+        sb.delete(sb.length() - 1, sb.length());
+        return sb.toString();
+    }
+}
